ba2989bfe4289076f2bcdbb6b4ed9c05814a6a07
tavor118 <andrew.moon.118@email.com>
2023-05-15 21:30:07 +0300

    Add swagger [PR #20]
    - Used https://github.com/tfranzel/drf-spectacular

--- src/apps/social/views.py
+++ src/apps/social/views.py
@@ -1,4 +1,7 @@
 from django.db.models import Prefetch
+from django_filters.rest_framework import DjangoFilterBackend
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import OpenApiParameter, extend_schema, extend_schema_view
 from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
@@ -26,6 +29,20 @@ class NoteViewSet(ModelViewSet):
         return Note.objects.filter(author=self.request.user)
 
 
+WITH_REPLIES_PARAMETER = OpenApiParameter(
+    name=WITH_REPLIES_QUERY_PARAM,
+    location=OpenApiParameter.QUERY,
+    description="Include replies in the response",
+    required=False,
+    type=OpenApiTypes.STR,
+)
+
+
+@extend_schema(tags=["comments"])
+@extend_schema_view(
+    list=extend_schema(parameters=[WITH_REPLIES_PARAMETER]),
+    retrieve=extend_schema(parameters=[WITH_REPLIES_PARAMETER]),
+)
 class CommentViewSet(ModelViewSet):
     queryset = (
         Comment.objects.select_related("author", "question")
@@ -44,8 +61,8 @@ class CommentViewSet(ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, AuthorOrReadOnly]
 
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_class = CommentFilter
-    filter_backends = [OrderingFilter]
     ordering_fields = ["id"]
 
     def get_queryset(self):
@@ -65,6 +82,7 @@ class CommentViewSet(ModelViewSet):
         return serializer_class(*args, **kwargs)
 
 
+@extend_schema(tags=["replies"])
 class CommentReplyViewSet(ModelViewSet):
     queryset = CommentReply.objects.select_related("author").all()
     serializer_class = CommentReplySerializer

4efc9a5ebe1a80887c4e9e6745887480ddb1931b
tavor118 <andrew.moon.118@email.com>
2023-05-15 21:30:07 +0300

    Add swagger [PR #20]
    - Used https://github.com/tfranzel/drf-spectacular

--- src/settings/components/auth.py
+++ src/settings/components/auth.py
@@ -12,7 +12,7 @@ REST_USE_JWT = True
 # dj_rest_auth settings
 REST_AUTH = {
     "USE_JWT": True,
-    # 'JWT_AUTH_COOKIE': 'my-app-auth',
+    "JWT_AUTH_COOKIE": "pj-jwt-auth-cookie",
     # 'JWT_AUTH_REFRESH_COOKIE': 'my-refresh-token',
     "JWT_AUTH_HTTPONLY": False,  # False needed for refresh_token
 }

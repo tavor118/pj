0fe92d69ad61ee6b15b252da629f7560e1d4930b
tavor118 <andrew.moon.118@email.com>
2023-05-21 17:20:28 +0300

    Add docker [PR #22]
    Used materials:
    - https://testdriven.io/blog/dockerizing-django-with-postgres-gunicorn-and-nginx/
    - https://testdriven.io/blog/django-docker-traefik/
    - https://testdriven.io/blog/docker-best-practices/
    - https://snyk.io/blog/best-practices-containerizing-python-docker/

--- src/settings/components/database.py
+++ src/settings/components/database.py
@@ -5,10 +5,10 @@ from src.settings.components.base import env
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": env("DB_NAME", default="pythonjunior"),
         "USER": env("DB_USER", default="postgres"),
-        "PASSWORD": env("DB_PASSWORD", default=""),
+        "PASSWORD": env("DB_PASSWORD", default="postgres"),
         "HOST": env("DB_HOST", default="127.0.0.1"),
         "PORT": env.int("DB_PORT", default=5432),
         "CONN_MAX_AGE": 3600,
